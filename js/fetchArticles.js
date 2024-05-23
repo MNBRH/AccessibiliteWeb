94aa18e9460f9e5bf7803f1686eec676aa941288
mh-lamine <mlamins.ngom@gmail.com>
2024-05-23 16:05:07 +0200

    Fix typos and remove unused files

--- js/fetchArticles.js
+++
@@ -1,5 +0,0 @@
-export default async function fetchArticles() {
-  const response = await fetch("../assets/articles.json");
-  const articles = await response.json();
-  return articles.articles;
-}
